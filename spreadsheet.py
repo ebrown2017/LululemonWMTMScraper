b1641df800bcbbbd96986b668ae2f7acca395554
ebrown2017 <48003662+ebrown2017@users.noreply.github.com>
2022-01-21 16:38:36 -0600

    Now parses properly to spreadsheet

--- spreadsheet.py
+++ spreadsheet.py
@@ -19,31 +19,47 @@ def parseToSpreadSheet(data):
     sheet.write(0, 2, 'Price')
     sheet.write(0, 3, 'Origional Price')
     sheet.write(0, 4, 'Available Sizes')
-    sheet.write(0, 5, 'Product Category')
-    sheet.write(0, 6, 'Colors Available')
-    sheet.write(0, 7, 'Skus Available')
-    sheet.write(0, 8, 'Link')
+    sheet.write(0, 5, 'Percent Off')
+    sheet.write(0, 6, 'Product Category')
+    sheet.write(0, 7, 'Colors Available')
+    sheet.write(0, 8, 'Skus Available')
+    sheet.write(0, 9, 'Link')
 
     def insertProduct(lineNum, product):
+        # create sku and color lists
         skus = []
         colors = []
         for variant in product['skuStyleOrder']:
             skus.append(variant['sku'])
             colors.append(variant['colorName'])
 
+        # calculate discount percentage
+        avgReg = sum(float(e) for e in product['listPrice']) / len(product['listPrice'])
+        avgDis = sum(float(e) for e in product['productSalePrice']) / len(product['productSalePrice'])
+        discount = 1 - (avgDis / avgReg)
+
         sheet.write(lineNum, 0, lineNum)
         sheet.write(lineNum, 1, product['displayName'])
-        sheet.write(lineNum, 2, str(product['productSalePrice']))
-        sheet.write(lineNum, 3, str(product['listPrice']))
-        sheet.write(lineNum, 4, str(product['allAvailableSizes']))
-        sheet.write(lineNum, 5, product['parentCategoryUnifiedId'])
-        sheet.write(lineNum, 6, str(colors))
-        sheet.write(lineNum, 7, str(skus))
-        sheet.write(lineNum, 8, "https://shop.lululemon.com" + product['pdpUrl'])
+        sheet.write(lineNum, 2, ', '.join('$' + str(e) for e in product['productSalePrice']))
+        sheet.write(lineNum, 3, ', '.join('$' + str(e) for e in product['listPrice']))
+        sheet.write(lineNum, 4, ', '.join(product['allAvailableSizes']))
+        sheet.write(lineNum, 5, "{:.2%}".format(discount))
+        sheet.write(lineNum, 6, product['parentCategoryUnifiedId'])
+        sheet.write(lineNum, 7, ', '.join(colors))
+        sheet.write(lineNum, 8, ', '.join(skus))
+        sheet.write(lineNum, 9, "https://shop.lululemon.com" + product['pdpUrl'])
 
     # loop through all products
-    for i, product in enumerate(data[2]['data']['categoryDetails']['products']):
-        insertProduct(i+1, product)
+    counter = 1
+    for product in data[2]['data']['categoryDetails']['products']:
+
+        # for some reason this is parsing products that do not have a sale price
+        # we want to exclude these so we execute this 
+        if product['productSalePrice'] == []:
+            continue
+
+        insertProduct(counter, product)
+        counter += 1
 
     # save spreadsheet
     fileData = getFileData(data[0], data[1])
